91df8f5a48def325151966f4e38193a3c98fb769
shawn8728 <wang61107@gmail.com>
2023-04-28 16:00:05 -0700

    Add home page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,15 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import CreatePost from '../views/CreatePost.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //     path: '/',
-    //     name: 'Home',
-    //     component: Home
-    // },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
     {
       path: '/login',
       name: 'Login',

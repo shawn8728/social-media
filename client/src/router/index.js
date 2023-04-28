70392f8de71547360d37bbdb75d547b573c6c26a
shawn8728 <wang61107@gmail.com>
2023-04-28 15:50:28 -0700

    Set up navbar and flowbite plugin

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,54 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from './components/Home.vue';
-import Register from './components/Register.vue';
-import Login from './components/Login.vue';
-import Post from './components/Post.vue';
-import CreatePost from './components/CreatePost.vue';
-
-const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    },
-    {
-        path: '/post',
-        name: 'Post',
-        component: Post
-    },
-    {
-        path: '/createpost',
-        name: 'CreatePost',
-        component: CreatePost
-    }
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
-
-router.beforeEach((to, from, next) => {
-    const publicPages = ['/login', '/register', '/'];
-    const authRequired = !publicPages.includes(to.path);
-    const loggedIn = localStorage.getItem('user');
-
-    if (authRequired && !loggedIn) {
-        return next('/login');
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // {
+    //     path: '/',
+    //     name: 'Home',
+    //     component: Home
+    // },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
+    // {
+    //     path: '/post',
+    //     name: 'Post',
+    //     component: Post
+    // },
+    // {
+    //     path: '/createpost',
+    //     name: 'CreatePost',
+    //     component: CreatePost
+    // }
+  ]
+})
 
-    next();
-});
+// router.beforeEach((to, from, next) => {
+//     const publicPages = ['/login', '/register', '/'];
+//     const authRequired = !publicPages.includes(to.path);
+//     const loggedIn = localStorage.getItem('user');
 
+//     if (authRequired && !loggedIn) {
+//         return next('/login');
+//     }
 
-export default router;
+//     next();
+// });
+
+export default router

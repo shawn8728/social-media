6e1465caaa141ee0665eb72682ec4a893e44fe40
shawn8728 <wang61107@gmail.com>
2023-04-28 15:29:01 -0700

    Add login page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,20 +1,54 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from './components/Home.vue';
+import Register from './components/Register.vue';
+import Login from './components/Login.vue';
+import Post from './components/Post.vue';
+import CreatePost from './components/CreatePost.vue';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes = [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+        path: '/',
+        name: 'Home',
+        component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/post',
+        name: 'Post',
+        component: Post
+    },
+    {
+        path: '/createpost',
+        name: 'CreatePost',
+        component: CreatePost
     }
-  ]
-})
+];
 
-export default router
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+    const publicPages = ['/login', '/register', '/'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
+
+    if (authRequired && !loggedIn) {
+        return next('/login');
+    }
+
+    next();
+});
+
+
+export default router;

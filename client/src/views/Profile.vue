16b1f9b0b0a75f6f002d4631d4e18ff714501085
shawn8728 <wang61107@gmail.com>
2023-05-01 02:46:40 -0700

    Add profile page

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -8,18 +8,19 @@
             <img
               class="shadow-xl rounded-full h-auto align-middle border-none lg:w-3/12 lg:order-2"
               alt="..."
-              src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg"
+              src="../assets/user.png"
             />
           </div>
           <div class="text-center mt-12">
             <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700">
-              User name
+              {{ userName }}
             </h3>
-            <!-- <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
               <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-              Los Angeles, California
+              <!-- Los Angeles, California -->
+              {{ userID }}
             </div>
-            <div class="mb-2 text-blueGray-600 mt-10">
+            <!-- <div class="mb-2 text-blueGray-600 mt-10">
               <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Solution Manager -
               Creative Tim Officer
             </div>
@@ -32,10 +33,7 @@
             <div class="flex flex-wrap justify-center">
               <div class="w-full lg:w-9/12 px-4">
                 <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
-                  An artist of considerable range, Jenna the name taken by Melbourne-raised,
-                  Brooklyn-based Nick Murphy writes, performs and records all of his own music,
-                  giving it a warm, intimate feel with a solid groove structure. An artist of
-                  considerable range.
+                  {{ userBio }}
                 </p>
                 <a href="#pablo" class="font-normal text-pink-500">Show more</a>
               </div>
@@ -49,4 +47,14 @@
 
 <script setup>
 import NavBar from '../components/NavBar.vue'
+import jwtDecode from 'jwt-decode'
+import { ref } from 'vue'
+
+const token = localStorage.getItem('token')
+const decodedToken = jwtDecode(token)
+const userData = decodedToken.userData
+
+const userName = ref(userData.username)
+const userID = ref(userData._id)
+const userBio = ref(userData.bio)
 </script>

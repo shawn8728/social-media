c4ff619740dad10cc92dd0d73aca1b6de53144c1
shawn8728 <wang61107@gmail.com>
2023-05-01 02:42:42 -0700

    Install fontawesome

--- client/src/main.js
+++ client/src/main.js
@@ -4,8 +4,23 @@ import router from './router/index.js'
 
 import './assets/styles.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faLinkedin, faGithub, faFacebook, faInstagram)
+
 const app = createApp(App)
 
 app.use(router)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 app.mount('#app')
+

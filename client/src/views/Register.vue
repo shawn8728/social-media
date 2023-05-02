8a5ae5adc1907d5525b107015a8eee333a721a10
shawn8728 <wang61107@gmail.com>
2023-05-01 19:17:08 -0700

    Adjust register page sizing

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,19 +1,19 @@
 <template>
-    <section class="bg-gray-50 dark:bg-gray-900">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <!-- <a
+    <section class="bg-white dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto max-w-screen-xl lg:py-16 lg:px-6"> 
+        <a
           href="#"
           class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
           <img
             class="w-8 h-8 mr-2"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            src="../components/icons/rocket.svg"
             alt="logo"
           />
-          Flowbite
-        </a> -->
+          SW
+        </a>
         <div
-          class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+          class="w-full bg-white rounded-lg border border-gray-200 shadow-md dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
         >
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1
